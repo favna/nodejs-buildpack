71413c2b525921c417251435b988e81bd577d685
Jeroen Claassens <jeroen.claassens@rws.nl>
2024-02-28 15:21:19 +0100

    chore: proper vendored support

--- src/nodejs/supply/supply.go
+++ src/nodejs/supply/supply.go
@@ -430,9 +430,16 @@ func (s *Supplier) ReadPackageJSON() error {
 
 		isYarnV1 := strings.HasPrefix(yarnVersion, "1")
 
+		s.Log.Info("yarnCacheFolder: ", yarnCacheFolder)
+		s.Log.Info("yarnVersion: ", yarnVersion)
+		s.Log.Info("yarnNodeLinker: ", yarnNodeLinker)
+		s.Log.Info("isYarnV1: ", isYarnV1)
+		s.Log.Info("stager builddir", s.Stager.BuildDir())
+
 		if !isYarnV1 && yarnNodeLinker == "pnp" {
-			s.Log.Protip("Yarn Berry is using Plug'n'Play (PnP) mode, detecting if Zero Installs is enabled by checking if the Yarn cache folder exists.", "https://yarnpkg.com/features/pnp")
+			s.Log.Info("Yarn Berry is using Plug'n'Play (PnP) mode, detecting if Zero Installs is enabled by checking if the Yarn cache folder exists. For more information visit https://yarnpkg.com/features/pnp")
 			if s.IsVendored, err = libbuildpack.FileExists(filepath.Join(s.Stager.BuildDir(), yarnCacheFolder)); err != nil {
+				s.Log.Info("yarn cache folder seemingly does not exist: ", err)
 				return err
 			}
 		}
